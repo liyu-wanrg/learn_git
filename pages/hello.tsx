2d9e7a55c44698c73125de20b6e4658c1dfad861
liyu <15517067680@163.com>
2021-10-29 15:42:54 +0800

    "fix"

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -2,10 +2,7 @@ export default function hello() {
   return (
     <div>
       <h1>hello, welcome</h1>
-<<<<<<< HEAD
-=======
       <h2>rebase again</h2>
->>>>>>> 1c0018d (test rebase)
     </div>
   );
 }
